df368d806b49f0404349df297179469c4c2d6d49
Sean Curtis <sean.curtis@tri.global>
2026-02-02 14:47:02 -0800

    Running "simulation"
    The agents new velocity is hard-coded to be the tangent of a circle
    centered on the world origin with radius equal to the agent's distance.
    
    Because we're doing first-order explicit integration, everyone floats to
    the outside.
    
    TODO:
     - Implement helbing's model.
       - Repulsive forces from walls.
       - Repulsive forces from other agents.
       - Integrate position and velocity.

--- warp/examples/core/sean_crowd.py
+++ warp/examples/core/sean_crowd.py
@@ -4,17 +4,30 @@ import numpy as np
 import warp as wp
 import warp.render
 
+@wp.kernel
+def compute_vel(p: wp.array(dtype=wp.vec2), v: wp.array(dtype=wp.vec2)):
+    id = wp.tid()
+    p0 = p[id]
+    # For now, circle the origin (explicit integration should cause badness).
+    p0_hat = wp.normalize(p0)
+    speed = 1.3
+    v[id] = wp.vec2(-p0_hat.y, p0_hat.x) * speed
+
+
+@wp.kernel
+def integrate(p: wp.array(dtype=wp.vec2), v: wp.array(dtype=wp.vec2), dt: float):
+    id = wp.tid()
+    p[id] += v[id] * dt
+
 
 class Example:
     def __init__(self, num_agents: int = 10):
         self.num_agents = num_agents
 
-        self.init_positions = np.random.rand(num_agents, 2) * 10.0 - 5
-        self.positions = np.copy(self.init_positions)
-        # self.positions = wp.array(self.init_positions, dtype=wp.vec2)
-        # self.velocities = wp.array(
-        #     [[0.0, 0.0] for _ in range(num_agents)], dtype=wp.vec2
-        # )
+        init_positions = np.random.rand(num_agents, 2) * 10.0 - 5
+        self.positions = wp.array(init_positions, dtype=wp.vec2)
+
+        self.velocities = wp.zeros_like(self.positions)
 
         self.agent_radius = 0.2
         self.desired_speed = 1.0
@@ -22,19 +35,22 @@ class Example:
         self.dt = 0.1
 
     def step(self):
-        shape = self.positions.shape
-        speed = 1.3  # m/s
-        velocities = (np.random.rand(shape[0], shape[1]) - 0.5) * speed
-        self.positions += velocities * self.dt
+        with wp.ScopedTimer("step"):
+            wp.launch(compute_vel, dim=self.num_agents,
+                    inputs=[self.positions, self.velocities])
+            wp.launch(integrate, dim=self.num_agents,
+                    inputs=[self.positions, self.velocities, self.dt])
 
     def step_and_render_frame(self, frame_num=None, agents=None):
         self.step()
         
         # Update agent patches
-        if agents:
-            for i, agent in enumerate(agents):
-                pos = self.positions[i]
-                agent.center = (pos[0], pos[1])
+        with wp.ScopedTimer("render"):
+            if agents:
+                positions = self.positions.numpy()
+                for i, agent in enumerate(agents):
+                    pos = positions[i]
+                    agent.center = (pos[0], pos[1])
 
         return agents
 
@@ -47,10 +63,12 @@ if __name__ == '__main__':
                          help="Override the default Warp device.")
     parser.add_argument('--num_frames', type=int, default=800,
                         help="Total number of frames.")
+    parser.add_argument('--num_agents', type=int, default=30,
+                        help="Number of agents in the crowd.")
     args = parser.parse_args()
 
     with wp.ScopedDevice(args.device):
-        example = Example(30)
+        example = Example(args.num_agents)
         import matplotlib.patches as patches
         import matplotlib.animation as anim
         import matplotlib.pyplot as plt
@@ -63,8 +81,9 @@ if __name__ == '__main__':
         ax.set_aspect('equal') # Important for circles to appear round
 
         # Add circles as patches
-        for pi in example.init_positions:
-            circle = patches.Circle(pi, radius=example.agent_radius, color='b')
+        for pi in example.positions.numpy():
+            c = np.random.rand(3,)
+            circle = patches.Circle(pi, radius=example.agent_radius, color=c)
             ax.add_patch(circle)
             agents.append(circle)
 
